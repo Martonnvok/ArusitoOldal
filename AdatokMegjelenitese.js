21e28084d784186df6c29e8b9a5cb9eaf4183294
Martonvok <martonsom@gmail.com>
2023-04-01 08:32:49 +0200

    first commit

---
+++ AdatokMegjelenitese.js
@@ -0,0 +1,26 @@
+export function megjelenitTabla(list) {
+    const article = document.querySelector("article");
+    let htmlkod =
+
+        `
+    <header><h1>Oláh pékség</h1></header> 
+    <table>
+    <thead>
+        <tr>
+            <td>Neve</td>
+            <td>db</td>
+            <td>Ár</td>
+        </tr>
+    </thead>
+    <tbody>`;
+    for (let index = 0; index < list.length; index++) {
+        htmlkod += `<tr>
+            <td>${list[index].nev}</td>
+            <td>${list[index].darab}</td>
+            <td>${list[index].ar}</td>
+            </tr>`;
+    }
+    htmlkod += ` </tbody >
+    </table >`;
+    article.innerHTML += htmlkod;
+}
---
+++ main.js
@@ -0,0 +1,10 @@
+import { megjelenitTabla } from "./AdatokMegjelenitese.js";
+window.addEventListener('load', init)
+function init() {
+  const PEKARUK = [{ nev: "Kakaóscsiga", darab: 20, ar: 350 }, { nev: "Zsemle", darab: 1, ar: 90 }, { nev: "Kifli", darab: 1, ar: 120 }, { nev: "Redbull", darab: 1, ar: 350 }, { nev: "Csokis tej", darab: 1, ar: 220 }];
+
+  megjelenitTabla(PEKARUK)
+
+}
+
+
