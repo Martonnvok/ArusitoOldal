2ec910b1b7d0c92992c56f3825d7b503a01573d6
Martonvok <martonsom@gmail.com>
2023-04-13 20:18:21 +0200

    MegjelenitTabla beillesztése az aM.js-be

--- AdatokMegjelenitese.js
+++ AdatokMegjelenitese.js
@@ -28,6 +28,51 @@ export function megjelenitDiv(list) {
      ARTICLE.innerHTML += htmlkod;
 }
 
+export function megjelenitTabla(list) {
+    const ARTICLE = document.querySelector("article");
+    const existingTable = ARTICLE.querySelector(".table");
+    if (existingTable) {
+        existingTable.remove();
+    }
+    let htmlkod =
+        `
+     
+     <div class="container mt-3">
+     <table class="table">
+     <thead class="table-dark">
+         <tr>
+             <td>Neve</td>
+             <td>db</td>
+             <td>Ár</td>
+             <td>Hozzáad/Elvessz</td>
+             <td></td>
+         </tr>
+     </thead>
+     <tbody>`;
+    for (let index = 0; index < list.length; index++) {
+        htmlkod += `<tr>
+             <td>${list[index].nev}</td>
+             <td>${list[index].darab}</td>
+             <td>${list[index].ar}Ft</td>
+             <td><button class="jobb">+</button><button class="bal">- </button></td>
+             <td><button class="kosar">Kosárba rakás</button></td>
+             </tr>`;
+    }
+    htmlkod += ` </tbody >
+     </table >`;
+     ARTICLE.innerHTML += htmlkod;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 export function nav() {
     const NAV = document.querySelector("nav");
