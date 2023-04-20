4d637555331f9d6f8c8c1a0c49dfe2ed51cdddd0
Martonvok <martonsom@gmail.com>
2023-04-20 19:57:32 +0200

    Bootstrap 5 formálása

--- AdatokMegjelenitese.js
+++ AdatokMegjelenitese.js
@@ -3,64 +3,57 @@ import { xKattintas } from "./GombokHsznalata.js";
 export function megjelenitDiv(list) {
   const ARTICLE = $("article");
   const DIVTORLES = ARTICLE.find(".div");
-  const MODALID = $("#myModal .modal-contenct")
+  const MODALID = $("#myModal .modal-content");
   DIVTORLES.remove();
 
-  let htmlkod = `<div class="semmi row"> `;
-  for (let index = 0; index < list.length; index++) {
-    htmlkod +=
-      `    
-        
-        <div class=" col-3 col-3">
-            <p>Neve: ${list[index].nev}</p>
-            <p>db:${list[index].darab}</p>
-            <p>Ár: ${list[index].ar}Ft</p>
-            <p>Kép: <img class="img-fluid" src="${list[index].kep}" alt="kep${index}"></p>
-            <p><button class="jobb btn btn-primary">+</button><button class="bal btn btn-primary">- </button></p>
-            <p><button class="kosar btn btn-success" id="id1${index}">Kosárba rakás</button></p>
-            <p><button class="class2 megnezem btn btn-info" data-bs-toggle="modal">Megnézem</button></p>
-        </div>`;
-  }
+  const HEADER = $("header");
+  let header = "";
+  header += `<h1 class="text-warning">Oláh Pékség</h1>`;
 
- 
+  let htmlkod = `<div class="semmi row bg-secondary p-3"> `;
+  for (let index = 0; index < list.length; index++) {
+    htmlkod += `
+      <div class="text-bg-dark col-3 col-3 rounded-4 border border-warning">
+        <p>Neve: ${list[index].nev}</p>
+        <p>db: ${list[index].darab}</p>
+        <p>Ár: ${list[index].ar}Ft</p>
+        <p>Kép: <img class="img-fluid border border-warning" src="${list[index].kep}" alt="kep${index}"></p>
+        <p><button class="jobb btn btn-primary ">+</button><button class="bal btn btn-primary">-</button></p>
+        <p><button class="kosar btn btn-success btn-sm" id="id1${index}">Kosárba</button></p>
+        <p><button type="button" class="btn btn-primary ">Mutat</button></p>
+      </div>`
+      ;
+  }
 
   /*
   const KOSARAK = $(".class2");
-  KOSARAK.on("click",function()  {
-  let id = even.target.id;
-  let bruh = `
-  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
-    Open modal
-  </button>
-  
-  <div class="modal" id="myModal">
-    <div class="modal-dialog">
-      <div class="modal-content">
-  
-        <div class="modal-header">
-          <h4 class="modal-title">${list[id].nev}</h4>
-          <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+  KOSARAK.on("click", function(event) {
+    let id = event.target.id.substring(3);
+    let bruh = `
+      <div class="modal" id="myModal">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">${list[id].nev}</h4>
+              <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <div class="modal-body">
+              <p>${list[id].leiras}</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Bezárás</button>
+            </div>
+          </div>
         </div>
-  
-        <div class="modal-body">
-          Modal body..
-        </div>
-  
-        <div class="modal-footer">
-          <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-        </div>
-  
       </div>
-    </div>
-  </div>
-
     `;
     MODALID.html(bruh);
-  })
-  megjegyzés: data-bs-target="#myModal"
-  */
+  });
+*/
   ARTICLE.html(htmlkod);
+  HEADER.append(header);
 }
+
 
 //Az admin, weblap elejét jeleníti meg
 
@@ -69,14 +62,14 @@ export function megjelenitTabla(adatok) {
   const ARTICLE = $("article");
 
   let htmlkod = `
-    <div class="container mt-3 semmi">
-      <table class="table" id="torol">
+    <div class="semmi">
+      <table class="table" id="torol table table-dark table-striped rounded-4">
         <thead class="table-dark">
           <tr>
-            <td>Neve</td>
-            <td>db</td>
-            <td>Ár</td>
-            <td></td>
+            <td class="table-dark">Neve</td>
+            <td class="table-dark">db</td>
+            <td class="table-dark">Ár</td>
+            <td class="table-dark"></td>
           </tr>
         </thead>
         <tbody>
@@ -84,10 +77,10 @@ export function megjelenitTabla(adatok) {
   for (let index = 0; index < adatok.length; index++) {
     htmlkod += `
           <tr>
-            <td>${adatok[index].nev}</td>
-            <td>${adatok[index].darab}</td>
-            <td>${adatok[index].ar}Ft</td>
-            <td><button class="btn btn-danger" id="x${index}">X</button></td>
+            <td class="table-secondary">${adatok[index].nev}</td>
+            <td class="table-light">${adatok[index].darab}</td>
+            <td class="table-secondary">${adatok[index].ar}Ft</td>
+            <td class="table-light"><button class="btn btn-danger" id="x${index}">X</button></td>
           </tr>
     `;
   }
@@ -102,20 +95,17 @@ export function megjelenitTabla(adatok) {
 
 //Navigáció, oldal linkek
 export function nav() {
-  const HEADER = $("header");
-  let header = "";
-  header += `<h1>Oláh Pékség</h1>`;
   const NAV = $("nav");
   let nav = "";
   nav +=
     `
-        <nav class=" navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" >Navigációk</a>
+        <nav class=" navbar navbar-expand-lg navbar-warning bg-warning rounded-end">
+  <div class="semmi  container-fluid">
+    <a class="navbar-brand " >Navigációk</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarText">
+    <div class="semmi collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="index.html">Főoldal</a>
@@ -133,11 +123,11 @@ export function nav() {
 export function adminHozzaad() {
   const HEADER = $("header");
   let header = "";
-  header += `<h1>Oláh Pékség</h1>`;
+  header += `<h1 class="text-warning">Oláh Pékség</h1>`;
   const ASIDE = $("aside");
   let aside = `
       <nav class="navbar navbar-expand-sm bg-light">
-        <div class="container-fluid">
+        <div class="container-fluid semmi">
           <ul class="navbar-nav">
             <li class="nav-item">
               <label>Név:</label>
@@ -168,7 +158,7 @@ export function hozzaAdas(adatok) {
   const ASIDE = $("aside");
   let aside = `
       <div class="semmi">
-        <button id="b">Hozzáadás</button>
+        <button class="kosar btn btn-success btn-sm" id="b">Hozzáadás</button>
       </div>
     `;
   ASIDE.append(aside);
@@ -188,9 +178,9 @@ export function hozzaAdas(adatok) {
 export function kosarMegjelenit() {
   const ASIDE = $("aside");
   let aside = `
-        <div class="semmi">
-        <p>Kosár össz:</p>
-        <p class="szamol">0</p>
+        <div class="semmi text-bg-dark col-3 col-3 rounded-4 border border-warning">
+          <p>Kosár összes tartalma:</p>
+          <p class="szamol">0</p>
         </div>
     `;
   ASIDE.append(aside);
@@ -198,7 +188,3 @@ export function kosarMegjelenit() {
   
 }
 
-export function mutat(list) {
-  
-    
-}
